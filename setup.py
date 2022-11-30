8d639f0a51e6b0dc3e0f4857508e626c2f7f577d
Nataleia Orlovska <nataleia.orlovska@gmail.com>
2022-11-30 22:46:07 +0200

    30_11_1

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     url='https://github.com/Nataleia08/clean_folder',
     author='Orlovska Nataliia',
     author_email='nataleia.orlovska@gmail.com',
+    package_dir={"clean_folder:clean_folder"}
     # license='MIT',
     packages=find_packages(include=["cleanfolder"]),
     entry_points={'console_scripts': [
