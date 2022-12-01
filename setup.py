6343f83f59e7112326ef1b4dc64c6c0404114d78
Nataleia Orlovska <nataleia.orlovska@gmail.com>
2022-12-01 20:58:08 +0200

    1_12
    Draft 2.0

--- setup.py
+++ setup.py
@@ -4,12 +4,18 @@ setup(
     name='clean_folder',
     version='1',
     description='Very useful code',
+    long_description="A function that sorts files and folders.", 
     url='https://github.com/Nataleia08/clean_folder',
     author='Orlovska Nataliia',
     author_email='nataleia.orlovska@gmail.com',
-    package_dir={"clean_folder:clean_folder"}
-    # license='MIT',
     packages=find_packages(include=["cleanfolder"]),
     entry_points={'console_scripts': [
         'clean-folder = clean_folder.clean:main']},
+    classifiers=[
+        "Python version :: Python :: 3", 
+        "Licence :: OSI :: MIT Licence"
+    ],
+    package_dir={"clean_folder":"clean_folder"},
+    python_requires = ">=3.7"
+    
 )
