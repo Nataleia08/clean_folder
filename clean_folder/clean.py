80655bcff4ff79895769ea8fd6e77380f859f05f
Nataleia Orlovska <nataleia.orlovska@gmail.com>
2022-12-01 21:31:32 +0200

    1_12_final
    Version 1.0

--- clean_folder/clean.py
+++ clean_folder/clean.py
@@ -214,6 +214,7 @@ def creating_folder(path: Path) -> list:
                  list_audio_path, list_docum_path, list_images_path]
     return [main_list_dir, list_path]
 
+
 def main():
     p = Path(input("Введіть шлях до папки:"))
     if p.is_dir():
@@ -225,4 +226,14 @@ def main():
     else:
         print("Це не шлях до папки!")
 
-main()
+
+# main()
+# p = Path(input("Введіть шлях до папки:"))
+# if p.is_dir():
+#     list_path = creating_folder(p)
+#     result = sorting(p, list_path)
+#     print("Сортування виконано успішно!")
+#     print("Список відомих розширень файлів у папці:", result[1])
+#     print("Список невідомих розширень у папці:", result[0])
+# else:
+#     print("Це не шлях до папки!")
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='clean_folder',
     version='1',
     description='Very useful code',
-    long_description="A function that sorts files and folders.", 
+    long_description="A function that sorts files and folders.",
     url='https://github.com/Nataleia08/clean_folder',
     author='Orlovska Nataliia',
     author_email='nataleia.orlovska@gmail.com',
@@ -12,10 +12,10 @@ setup(
     entry_points={'console_scripts': [
         'clean-folder = clean_folder.clean:main']},
     classifiers=[
-        "Python version :: Python :: 3", 
+        "Python version :: Python :: 3",
         "Licence :: OSI :: MIT Licence"
     ],
-    package_dir={"clean_folder":"clean_folder"},
-    python_requires = ">=3.7"
-    
+    package_dir={"clean_folder": "clean_folder"},
+    python_requires=">=3.7"
+
 )
